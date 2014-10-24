ed91ff68b2d3a2751f4e48a501596c36c127ad2f
Lili Shi <lilidotshi@gmial.com>
2014-10-24 17:09:15 -0400

    Added the ability to page between sections
    User can now swipe through sections

--- src/com/eightbyeight/irblaws/ReaderActivity.java
+++ src/com/eightbyeight/irblaws/ReaderActivity.java
@@ -1,12 +1,18 @@
 package com.eightbyeight.irblaws;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import com.eightbyeight.irblaws.jsonobjects.Laws;
 
@@ -24,11 +30,26 @@ public class ReaderActivity extends ActionBarActivity implements
 	 * {@link #restoreActionBar()}.
 	 */
 	private CharSequence mTitle;
+	private int mLawNumber;
 	
 	private Laws mLaws;
-	
-	private int mLawNumber;
-	private int mSectionNumber;
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static int num_pages = 0;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
+
+	private int mSectionNumber = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,36 +58,40 @@ public class ReaderActivity extends ActionBarActivity implements
 		// Set up the drawer.
 		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
 				(DrawerLayout) findViewById(R.id.drawer_layout));
-
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setCurrentItem(mSectionNumber);
 	}
 	
 	@Override
 	public void onNavigationDrawerItemSelected(int position) {
-		// update the main content by replacing fragments
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		ContentFragment contentFragment = new ContentFragment();
-		
 		//Set up our variables
+		setUpVariablesForBundle(position);
+		mTitle = mLaws.getLaws().get(mLawNumber).getLawName();
+		
+		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+
+		if (mPager != null)
+			mPager.setCurrentItem(position, true);
+
+	}
+
+	private Bundle setUpVariablesForBundle(int position) {
 		mLaws = (Laws) getIntent().getExtras().get("laws");
 		mLawNumber = getIntent().getExtras().getInt("lawnumber");
-		mTitle = mLaws.getLaws().get(mLawNumber).getContent().get(position).getSectionName();
 		mSectionNumber = position;
-		
+		num_pages = mLaws.getLaws().get(mLawNumber).getContent().size();
+		String title = mLaws.getLaws().get(mLawNumber).getContent().get(position).getSectionName();
+
 		//Pass on arguments to our new fragment
 		Bundle args = getIntent().getExtras();
 		args.putSerializable("laws", mLaws);
 		args.putInt("lawnumber", mLawNumber);
 		args.putInt("sectionnumber", mSectionNumber);
-		args.putString("sectiontitle", mTitle.toString());
-		contentFragment.setArguments(args);
-		
-		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.navigation_drawer);
-
-		fragmentManager
-				.beginTransaction()
-				.replace(R.id.container,
-						contentFragment).commit();
+		args.putString("sectiontitle", title.toString());
+		return args;
 	}
 
 	public void restoreActionBar() {
@@ -100,4 +125,27 @@ public class ReaderActivity extends ActionBarActivity implements
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	/**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+        	Bundle args = setUpVariablesForBundle(position);
+        	ContentFragment contentFragment = new ContentFragment();
+        	contentFragment.setArguments(args);
+            return contentFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return num_pages;
+        }
+    }
 }
